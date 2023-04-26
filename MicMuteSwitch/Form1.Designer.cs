7067390f334646a429f14564e8f07930283a22b9
kimsunbal <kimsunbal@naver.com>
2023-04-26 22:13:48 +0900

    초기 커밋.

---
+++ MicMuteSwitch/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MicMuteSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Window;
+            label1.Location = new Point(32, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 0;
+            label1.Text = "마이크 이름";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.Window;
+            label2.Location = new Point(151, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.Window;
+            label3.Location = new Point(32, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 15);
+            label3.TabIndex = 2;
+            label3.Text = "마이크 상태";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.Window;
+            label4.Location = new Point(151, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 3;
+            label4.Text = "label4";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(287, 33);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 67);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 136);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ MicMuteSwitch/Form1.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualBasic;
+using NAudio.CoreAudioApi;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MicMuteSwitch
+{
+    public partial class Form1 : Form
+    {
+        // MMDeviceEnumerator 객체 생성
+        MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+
+        // RegisterHotKey 함수
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        // UnregisterHotKey 함수
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // 단축키 ID (Toggle Key)
+        private const int HOTKEY_ID = 0;
+
+        public void CheckMicStatus()
+        {
+            // 기본 입력 장치 (마이크) 가져오기
+            MMDevice device = (MMDevice)enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            //생성된 스레드가 아닌 다른 스레드에서 호출될 경우 true
+            var deviceName = device.DeviceFriendlyName;
+            if (label2.InvokeRequired)
+            {
+                Console.WriteLine("out thread");
+                label2.Invoke(new MethodInvoker(delegate ()
+                {
+                    label2.Text = deviceName;
+
+                }));
+                if (device.AudioEndpointVolume.Mute)
+                {
+                    label4.Invoke((MethodInvoker)(() => label4.Text = "Off"));
+                    button1.Invoke((MethodInvoker)(() => button1.Text = "마이크 켜기(&X)"));
+                    button1.Invoke((MethodInvoker)(() => notifyIcon1.BalloonTipText = "마이크 꺼짐"));
+
+                }
+                else
+                {
+                    label4.Invoke((MethodInvoker)(() => label4.Text = "On"));
+                    button1.Invoke((MethodInvoker)(() => button1.Text = "마이크 끄기(&X)"));
+                    button1.Invoke((MethodInvoker)(() => notifyIcon1.BalloonTipText = "마이크 켜짐"));
+                }
+            }
+            else
+            {
+                Console.WriteLine("in thread");
+                label2.Text = device.DeviceFriendlyName;
+                if (device.AudioEndpointVolume.Mute)
+                {
+                    label4.Text = "Off";
+                    button1.Text = "마이크 켜기(&X)";
+                    notifyIcon1.BalloonTipText = "마이크 꺼짐";
+                }
+                else
+                {
+                    label4.Text = "On";
+                    button1.Text = "마이크 끄기(&X)";
+                    notifyIcon1.BalloonTipText = "마이크 켜짐";
+                }
+            }
+
+            notifyIcon1.ShowBalloonTip(0);
+
+        }
+
+        public void ToggleMicOnOff()
+        {
+            var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            // 마이크 On/Off
+            device.AudioEndpointVolume.Mute = !device.AudioEndpointVolume.Mute;
+
+            // 마이크 상태 Label 값 변경
+            //CheckMicStatus();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+                device.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+                CheckMicStatus();
+            }
+            catch (Exception ex)
+            {
+                // 예외 처리
+                if (ex is COMException)
+                {
+                    MessageBox.Show("연결된 기본 마이크가 없습니다.", "장치 없음", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message, "알 수 없는 에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                // 예외 발생 시 강제 종료
+                Environment.Exit(1);
+            }
+        }
+
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            Console.WriteLine("VolumeNotification: {0}", data.MasterVolume);
+            // 마이크 상태 변경 시 실행할 로직 구현
+            CheckMicStatus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("button click");
+            ToggleMicOnOff();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                // 단축키 처리 코드 작성
+                ToggleMicOnOff();
+                Console.WriteLine("토글 실행");
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ctrl + Alt + Shift + M 단축키 등록
+            Console.WriteLine(" 단축키 등록");
+            RegisterHotKey(this.Handle, HOTKEY_ID, 0x0, (int)Keys.Scroll);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            // 등록한 단축키 해제
+            Console.WriteLine(" 단축키 해제");
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+    }
+}
